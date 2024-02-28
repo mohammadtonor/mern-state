657b085d80cc3e872c3fa510c09b51c9d1682135
mohammadtonor <mtonor1368@gmail.com>
2024-02-28 21:04:31 +0330

    create pages and routed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { SignUp } from './page/SignUp'
+import { SignIn } from './page/SignIn'
+import { About } from './page/About'
+import { Profile } from './page/Profile'
 
 function App() {
 
   return (
-    <div>Ract app</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
