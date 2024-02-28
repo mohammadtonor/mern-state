978aa6e8f74d768d7941145194bb6e788ef12faa
mohammadtonor <mtonor1368@gmail.com>
2024-02-28 22:54:49 +0330

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_URL)
 
 const app = express();
 
