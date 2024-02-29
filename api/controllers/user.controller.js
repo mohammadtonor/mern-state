fda566b869ac61789e5d449c2f30c3ca596c7109
mohammadtonor <mtonor1368@gmail.com>
2024-03-01 01:32:06 +0330

    update user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs';
+import User from './../models/user.model.js'
+
 export const test = (req , res) => {
     res.json({
         message: 'Api route test work'
     })
+}
+
+export const updateUser = async (req , res, next) => {
+    const id = req.params.id;
+    console.log(req.user, id);
+    if(req.user.id !== id) return next(errorHandler(401, "You can update your own profile"))
+    try {
+        let hashedPassword;
+        if(req.body.password) {
+            hashedPassword = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPassword,
+                avatar: req.body.avatar
+            }
+        }, {new: true});
+
+        const {password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);   
+    }
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGO_DB_URL)
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
